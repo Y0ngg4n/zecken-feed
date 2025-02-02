116ffc7ec2bb79713755c50645540da0e2a5ab62
Yonggan <yonggan@obco.pro>
2025-02-02 01:48:20 +0100

    Add signalbot

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from datetime import datetime, date
 import time
 from feedgen.feed import FeedGenerator
 from fastapi import FastAPI, Response
+from dotenv import load_dotenv
+from signalbot import SignalBot
+import asyncio
+
+load_dotenv()
 
 counter_file = "./data/counter.txt"
 data_file = "./data/data.csv"
@@ -19,13 +24,13 @@ class Collector:
     demos = []
     feed: str
 
-    def start(self):
+    async def start(self):
         self.demo = []
 
         if not os.path.isdir(data_dir):
             os.makedirs(data_dir, exist_ok=True)
         taz = Taz()
-        taz.get_data()
+        await taz.get_data()
 
     def convert_data_to_rss(self):
         fg = FeedGenerator()
@@ -51,13 +56,19 @@ class Demo:
     date: date
     time: str
     link: str
+    latitude: float
+    longitude: float
 
-    def __init__(self, place, startPoint, date, time, link) -> None:
+    def __init__(
+        self, place, startPoint, date, time, link, latitude, longitude
+    ) -> None:
         self.place = place
         self.startPoint = startPoint
         self.date = date
         self.time = time
         self.link = link
+        self.latitude = latitude
+        self.longitude = longitude
 
     def getId(self):
         date = self.date.strftime("%d.%m.%Y")
@@ -87,8 +98,8 @@ class Taz(Scraper):
         if os.path.isfile(counter_file):
             with open(counter_file, "r") as f:
                 try:
-                    self.counter = int(f.readline().strim())
-                    print("Updated Counter to " + self.counter)
+                    self.counter = int(f.readline().strip())
+                    print("Updated Counter to " + str(self.counter))
                 except Exception as e:
                     print(e)
                     pass
@@ -96,7 +107,7 @@ class Taz(Scraper):
     def updateUrl(self) -> str:
         self.url = f"https://datawrapper.dwcdn.net/p3Ttm/{self.counter}/dataset.csv"
 
-    def get_data(self):
+    async def get_data(self):
         global counter_file
         global data_file
         global old_data_file
@@ -130,6 +141,27 @@ class Taz(Scraper):
             with open(file=old_data_file, mode="r") as old:
                 old_csv = list(csv.reader(old.read().splitlines(), delimiter=","))
                 diff = list(set(map(tuple, csv_response)) - set(map(tuple, old_csv)))
+                print(diff)
+                bot = SignalBot(
+                    {
+                        "signal_service": os.environ["SIGNAL_SERVICE"],
+                        "phone_number": os.environ["PHONE_NUMBER"],
+                    }
+                )
+                await bot.send(os.environ["GROUP"], "Test")
+                for item in diff:
+                    await bot.send(
+                        os.environ["GROUP"],
+                        item[0]
+                        + "\n"
+                        + item[2]
+                        + " "
+                        + item[3]
+                        + "\n"
+                        + item[1]
+                        + "\n"
+                        + item[6],
+                    )
 
         for i in range(1, len(csv_response)):
             Collector.demos.append(
@@ -137,6 +169,8 @@ class Taz(Scraper):
                     csv_response[i][0],
                     csv_response[i][1],
                     datetime.strptime(csv_response[i][2].strip(), "%d.%m.%Y"),
+                    csv_response[3],
+                    csv_response[i][6],
                     csv_response[i][3],
                     csv_response[i][4],
                 )
@@ -150,6 +184,6 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    collector.start()
+    await collector.start()
     collector.convert_data_to_rss()
     return Response(content=collector.feed, media_type="application/xml")
